f4dc0717db8283ddd1b6c757c1a1152d88503fc8
Nathanael Fuog <nathanael@fuog.net>
2021-12-21 08:02:34 +0100

    added az auto-complete support

--- additionals/fuog-default.zsh
+++ additionals/fuog-default.zsh
@@ -30,6 +30,11 @@ command -v batcat >/dev/null 2>&1 && alias cat=batcat
 # for resetting some audio stuff
 command -v alsa >/dev/null 2>&1 && alias audio-reload="sudo alsa force-reload"
 
+# adding Azure autocompletion https://gastaud.io/en/article/azure-cli-autocomplete/
+command -v alsa >/dev/null 2>&1 && \
+  test -f "/etc/bash_completion.d/azure-cli" >/dev/null 2>&1 && \
+    source /etc/bash_completion.d/azure-cli
+
 # Tarra-stuff
 command -v terraform >/dev/null 2>&1 && alias tf="terraform"
 command -v terragrunt >/dev/null 2>&1 && alias tg="terragrunt"
