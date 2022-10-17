296f08bfa459a74963635737dbe329ce4a25a371
Nathanael Fuog <nathanael@fuog.net>
2022-10-17 19:52:24 +0200

    added git-clean alias

--- main-rc.zsh
+++ main-rc.zsh
@@ -164,3 +164,13 @@ function scp_wrap {
   esac; done
   command scp "${(@)args}"
 }
+
+# Cleaning Repo in a oneliner
+alias git-clean="git checkout master \
+  || git checkout main \
+  && git pull --all --prune \
+  && git fetch -p \
+  && for branch in $(\
+    git for-each-ref --format '%(refname) %(upstream:track)' refs/heads \
+    | awk '$2 == "[gone]" {sub("refs/heads/", "", $1); print $1}')\
+    ; do git branch -D $branch ; done"
