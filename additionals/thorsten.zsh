1a3590329e3b3ee520e5cbccdf94390010bc71fa
pritho <23241510+pritho@users.noreply.github.com>
2022-12-27 14:12:28 +0100

    Delete thorsten.zsh

--- additionals/thorsten.zsh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-# Script sourcing check
-test -z "$PS1" \
-	&& echo -e "This script \033[00;31mshould be sourced\033[0m not executed" && exit 1
-
-#Set base64 aliases for MacOS
-alias base64encode="openssl base64 -e <<<"
-alias base64decode="openssl base64 e <<<"
